a907e9b7346b72b163f1bd3a5ea4555316008242
Knasdude <06joesta@gafe.molndal.se>
2025-01-08 11:27:54 +0100

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<String> Books;
+
+    public Library(){
+        this.Books = new ArrayList<>();
+    }
+
+        }
---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.addBook("Lord Rings");
+
+        }
+    }
