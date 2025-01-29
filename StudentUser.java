38bd89f0385d634fc675107be29f3d31e5471061
froggelito <06rohshe@gafe.molndal.se>
2025-01-29 13:37:39 +0100

    Update StudentUser.java

--- StudentUser.java
+++ StudentUser.java
@@ -1,11 +1,7 @@
-import java.util.List;
-
 public class StudentUser extends User {
-    private final String name;
 
-    public StudentUser (int password, String emailAddress, List<BasicBook> borrowedBooks, String name){
-        super(password, emailAddress, borrowedBooks);
-        this.name = name;
+    public StudentUser (String password, String emailAddress){
+        super(password, emailAddress);
     }
 
 }
