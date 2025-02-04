9b5b42052523e0011397eed0555e643243313963
froggelito <06rohshe@gafe.molndal.se>
2025-02-05 00:00:03 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,33 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public abstract class User {
     public String password;
     public String emailAddress;
-    public List<String> borrowedBooks = new ArrayList<>();
-    public Boolean punishment = false;
+    public List<BasicBook> borrowedBooks = new ArrayList<>();
 
     public User (String password, String emailAddress){
         this.password = password;
         this.emailAddress = emailAddress;
+        saveUser();
     }
 
-    public boolean login (String emailVerification, String passwordVerification) {
-        return passwordVerification.equals(password) && emailVerification.equals(emailAddress);
+    public void saveUser () {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Users.txt", true))) {
+            writer.write("User: " + emailAddress + " Password: " + password + "\n");
+            writer.flush();
+        }
+        catch (IOException e) {
+            System.out.println("Error writing user data: " + e.getMessage());
+            }
+        }
+
+    public String getPassword () {
+        return password;
     }
 
-
-    public String getName () {
+    public String getEmailAddress () {
         return emailAddress;
     }
 
-    public List<String> getBorrowedBooks () {
-        if (borrowedBooks.size() > 5) {//lånetid
-            punishment = true;
-        }
-
+    public List<BasicBook> getBorrowedBooks () {
         return borrowedBooks;
     }
 
-
 }
+
