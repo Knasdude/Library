1ade15b6ee9596a636eda54d8f7196b28fbe17a9
froggelito <06rohshe@gafe.molndal.se>
2025-02-09 13:24:29 +0100

    Update Library.java

--- Library.java
+++ Library.java
@@ -6,19 +6,6 @@ import java.util.Scanner;
 
 public class Library {
 
-    public String availableBooks () {
-        StringBuilder books = new StringBuilder();
-        File file = new File("C:\\Users\\06rohshe\\IdeaProjects\\Library\\src\\Books.txt");
-        try {
-            Scanner booksFileReader = new Scanner(file);
-            while (booksFileReader.hasNextLine()) {
-                books.append(booksFileReader.nextLine()).append("\n");
-            }
-            return "tillgängliga böcker" + books.toString().trim();
-        } catch (FileNotFoundException e) {
-            return e.getMessage();
-        }
-    }
     public String borrowBook (String email, String password, StudentUser user, BasicBook book) {
         if (user.login(email, password) && user.borrowedBooks.size() < 5){
             user.borrowedBooks.add(book.getISBN());
